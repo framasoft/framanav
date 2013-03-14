213c3eccd8dc4b9287ed7d34aaa75f80590dd659
Framasoft <technique@framasoft.net>
2013-03-14 17:49:26 +0100

    Corrections diverses

--- nav.php
+++ nav.php
@@ -49,6 +49,7 @@ if (!defined('FNAV_CONFIG')) {
 							<li><a href="http://framasoft.org" rel="popover" data-content="Le r&eacute;seau Framasoft" title="Framasoft"><i class="icon-home"></i>&nbsp;Framasoft</a></li>
 							<li class="divider"></li>
 							<li><a href="http://framasoft.org" rel="popover" data-content="L'annuaire de logiciels libres. <div class='framanav_scr framanav_framasoft'></div>" title="Framalibre"><i class="icon-list"></i>&nbsp;Framalibre</a></li>
+							<li><a href="http://framastart.org" rel="popover" data-content="Le meilleur des logiciels libres sur une seule page." title="Framastart"><i class="icon-bookmark"></i>&nbsp;Framastart</a></li>
 							<li><a href="http://framadvd.org" rel="popover" data-content="Compilations de logiciels libres sur DVD. <div class='framanav_scr framanav_framadvd'></div>" title="FramaDVD"><i class="icon-forward"></i>&nbsp;FramaDVD</a></li>
 							<li><a href="http://framapack.org" rel="popover" data-content="Installation rapide de multiples logiciels libres. <div class='framanav_scr framanav_framapack'></div>" title="Framapack"><i class="icon-download-alt"></i>&nbsp;Framapack</a></li>
 							<li><a href="http://framakey.org" rel="popover" data-content="Applications nomades pour cl&eacute;s USB. <div class='framanav_scr framanav_framakey'></div>" title="Framakey"><img src="<?php echo FNAV_RPATH; ?>/img/small_usb.png" />&nbsp;Framakey</a></li>
@@ -61,7 +62,7 @@ if (!defined('FNAV_CONFIG')) {
                         <ul class="dropdown-menu">
 							<li><a href="http://framablog.org" rel="popover" data-content="Le blog de Framasoft. <div class='framanav_scr framanav_framablog'></div>" title="Framablog"><i class="icon-pencil"></i>&nbsp;Framablog</a></li>
 							<li><a href="http://framabook.org" rel="popover" data-content="Collection de livres libres.  <div class='framanav_scr framanav_framabook'></div>" title="Framabook"><i class="icon-book"></i>&nbsp;Framabook</a></li>
-							<li><a href="http://framalang.org" rel="popover" data-content="Collection de livres libres.  <div class='framanav_scr framanav_framalang'></div>" title="Framalang"><i class="icon-book"></i>&nbsp;Framalang</a></li>
+							<li><a href="http://www.framablog.org/index.php/pages/framalang" rel="popover" data-content="La communaut&eacute; de traducteurs du r&eacute;seau Framalang." title="Framalang"><i class="icon-flag"></i>&nbsp;Framalang</a></li>
                       		<li><a href="http://framatube.org" rel="popover" data-content="Les vid&eacute;os du libre.<div class='framanav_scr framanav_framatube'></div>" title="Framatube"><i class="icon-facetime-video"></i>&nbsp;Framatube</a></li>
 							<li><a href="http://framazic.org" rel="popover" data-content="Tout savoir sur la musique libre.<div class='framanav_scr framanav_framazic'></div>" title="Framazic"><i class="icon-music"></i>&nbsp;Framazic</a></li>
 					
@@ -87,7 +88,7 @@ if (!defined('FNAV_CONFIG')) {
 							<ul class="dropdown-menu">
 								<li><a href="http://framalab.org" rel="popover" data-content="Le laboratoire de Framasoft" title="Framalab"><i class="icon-refresh"></i>&nbsp;Framalab</a></li>
 								<li><a href="http://wiki.framasoft.org" rel="popover" data-content="Le wiki de Framasoft. <div class='framanav_scr framanav_framawiki'></div>" title="Framawiki"><i class="icon-font"></i>&nbsp;Framawiki</a></li>
-								<li><a href="http://framacode.org" rel="popover" data-content="T&eacute;l&eacute;charger nos applications ou contribuer &acute; leur d&eacute;veloppement.<div class='framanav_scr framanav_framacode'></div>" title="Framacode"><i class="icon-wrench"></i>&nbsp;Framacode</a></li>
+								<li><a href="http://framacode.org" rel="popover" data-content="T&eacute;l&eacute;charger nos applications ou contribuer &acute; leur d&eacute;veloppement." title="Framacode"><i class="icon-wrench"></i>&nbsp;Framacode</a></li>
 								<li><a href="http://forum.framasoft.org" rel="popover" data-content="Les forums de Framasoft. <div class='framanav_scr framanav_framagora'></div>" title="Framagora"><i class="icon-comment"></i>&nbsp;Framagora</a></li>
 								<li><a href="http://soutenir.framasoft.org" rel="popover" data-content="Framasoft ne vit que gr&acirc;ce &acute; vos dons.<br/>Soutenez-nous ! <div class='framanav_scr framanav_soutenir'></div>" title="Faire un don"><i class="icon-gift"></i>&nbsp;Faire un don</a></li>
 								<li class="divider"></li>
@@ -99,11 +100,11 @@ if (!defined('FNAV_CONFIG')) {
                       </li>     
  
 					<form class="navbar-search pull-right" action="http://www.google.com/cse"> 
-						<input type="hidden" value="017931506014566040656:iqp-lrqbqlu" name="cx"/> 
+						<input type="hidden" value="002138387073339262058:qhubrzuwlhg" name="cx"/> 
 						<input type="text" size="25" name="q" placeholder="Rechercher" class="search-query span2"/> 
 						<input type="submit" value="Go" name="sa" style="display:none"/> 
 					</form> 
-					<script src="http://www.google.com/coop/cse/brand?form=searchbox_017931506014566040656%3Aiqp-lrqbqlu&amp;lang=fr" type="text/javascript"></script>
+					<script src="http://www.google.com/coop/cse/brand?form=searchbox_002138387073339262058:qhubrzuwlhg&amp;lang=fr" type="text/javascript"></script>
 
                     <ul class="nav pull-right">
                       <li class="dropdown">
@@ -113,8 +114,9 @@ if (!defined('FNAV_CONFIG')) {
 							<li><a href="http://identi.ca/framasoft" rel="popover" data-content="Notre compte de microblogging <strong>libre</strong> !" title="Identi.ca / Framasoft"><i class="icon-search"></i>&nbsp;Identi.ca Framasoft</a></li>
 							<li><a href="http://twitter.com/framaka" rel="popover" data-content="Le compte de microblogging d'aKa." title="Twitter / aKa"><i class="icon-user"></i>&nbsp;Twitter Framaka</a></li>
 							<li><a href="https://www.facebook.com/framasoft" data-content="Framasoft est <em>aussi</em> sur Facebook (oui, on sait...)." title="Facebook / Framasoft"><i class="icon-thumbs-up"></i>&nbsp;Facebook</a></li>
-							<li><a href="http://soutenir.framasoft.org/newsletter" data-content="Rejoignez nos 5000 abonn&eacute;s &acute; notre lettre d'informations." title="Lettre d'informations"><i class="icon-bullhorn"></i>&nbsp;Newsletter</a></li>
+							<li><a href="http://soutenir.framasoft.org/newsletter" data-content="Rejoignez nos 5000 abonn&eacute;s &agrave; notre lettre d'informations." title="Lettre d'informations"><i class="icon-bullhorn"></i>&nbsp;Newsletter</a></li>
 							<li><a href="http://framapack.org/~framaflux/rss.php"  data-content="Flux RSS agr&eacute;g&eacute; des diff&eacute;rents sites." title="Flux RSS"><img src="<?php echo FNAV_RPATH; ?>/img/small_rss.png" />&nbsp;Flux RSS</a></li>
+							<li><a href="http://fr.wikipedia.org/wiki/Framasoft"  data-content="Parcourez le (long) article Framasoft sur (l'incontournable) encyclop&eacute;die libre Wikip&eacute;dia." title="Framasoft sur Wikip&eacute;dia"><img src="<?php echo FNAV_RPATH; ?>/img/wp_logo.png" />&nbsp;Page Wikip&eacute;dia</a></li>
 							<li><a href="http://contact.framasoft.org" data-content="Vous souhaitez prendre contact avec nous ?" title="Contacter Framasoft"><i class="icon-envelope"></i>&nbsp;Contact</a></li>
                         </ul>
                       </li>
