74e9421d546a553849af4df33edbc2df8b0c7259
pyg77 <pyg+github@framasoft.net>
2013-06-18 16:23:27 +0200

    Update config.default.php
    typo

--- conf/config.default.php
+++ conf/config.default.php
@@ -1,6 +1,6 @@
 <?php
 function myDefine($name, $value) {
-    if !(defined($name)) define($name, $value));
+    if (!defined($name)) define($name, $value);
 }
 
 # Flag de config
@@ -15,7 +15,7 @@ myDefine("FNAV_RPATH", "/framanav");
 # Chemin du zip sur github
 myDefine("FNAV_ZIP_URL", "https://github.com/framasoft/framanav/archive/master.zip");
 
-# user/group de la nav (utilisé par le script deploy-git uniquement
+# user/group de la nav (utilisï¿½ par le script deploy-git uniquement
 myDefine("FNAV_USER", "fsoft");
 myDefine("FNAV_GROUP", "users");
 
@@ -37,39 +37,39 @@ myDefine("FNAV_LOCAL_BOOTSTRAP_CSS", true);
 # Doit-on charger le CSS Responsive de Bootstrap ?
 myDefine("FNAV_LOCAL_BOOTSTRAP_RESPONSIVE_CSS", true);
 
-# ========= Système d'extras =======
+# ========= Systï¿½me d'extras =======
 
-# Doit-on charger un fichier CSS supplémentaire ?
-# Par défaut à false
+# Doit-on charger un fichier CSS supplï¿½mentaire ?
+# Par dï¿½faut ï¿½ false
 # Sinon, chemin du fichier. ex: define("FNAV_EXTEND_CSS", FNAV_RPATH."/extra.css");
 myDefine("FNAV_EXTRA_CSS", false);
 
-# Ajout de code spécifique en début de nav (cf $fnav_extra_start)
+# Ajout de code spï¿½cifique en dï¿½but de nav (cf $fnav_extra_start)
 myDefine("FNAV_EXTRA_START", false);
 
-# Ajout de code spécifique en fin de nav (cf $fnav_extra_end)
+# Ajout de code spï¿½cifique en fin de nav (cf $fnav_extra_end)
 myDefine("FNAV_EXTRA_END", false);
 
-# ========= Système d'alerte =======
+# ========= Systï¿½me d'alerte =======
 
 # Doit-on afficher une alerte ?
 myDefine("FNAV_EXTRA_ALERT", false);
 
 # Doit-on afficher une fenetre modale ?
 myDefine("FNAV_EXTRA_ALERT_MODAL", false);
-$fnav_extra_alert_modal_force = "true"; //forcer l'affichage de la fenetre à la première visite
-$alert_cookie_time = 0.05; // durée du cookie, en heures.
-$fnav_extra_alert_txt_title = "Attention, ce site sera bientot mis-à-jour !"; // contenu par défaut du contenu de l'alerte sous la nav.
+$fnav_extra_alert_modal_force = "true"; //forcer l'affichage de la fenetre ï¿½ la premiï¿½re visite
+$alert_cookie_time = 0.05; // durï¿½e du cookie, en heures.
+$fnav_extra_alert_txt_title = "Attention, ce site sera bientot mis-ï¿½-jour !"; // contenu par dï¿½faut du contenu de l'alerte sous la nav.
 
 
 # Doit-on lire le contenu de l'alerte depuis des fichiers (locaux ou distants)  ?
 myDefine("FNAV_EXTRA_ALERT_TXT_FILES", false);
 # Chemin du fichier pour le contenu de l'alerte sous la nav
-# Par défaut : dirname(__FILE__)."/alert_title.txt"; 
+# Par dï¿½faut : dirname(__FILE__)."/alert_title.txt"; 
 # Mais il est possible d'envisager un chemin distant type "http://framasite.org/nav/alert_title.txt
 $fnav_extra_alert_txt_files_title = dirname(__FILE__)."/alert_title.txt"; 
 # Chemin du fichier pour le contenu de la fenetre modale
-# Par défaut : dirname(__FILE__)."/alert_content.txt"; 
+# Par dï¿½faut : dirname(__FILE__)."/alert_content.txt"; 
 # Mais il est possible d'envisager un chemin distant type "http://framasite.org/nav/alert_content.txt
 $fnav_extra_alert_txt_files_content = dirname(__FILE__)."/alert_content.txt";
 
@@ -115,12 +115,12 @@ $fnav_extra_alert_cookie = <<<EOAB
 jQuery.noConflict();
 jQuery(document).ready(function() {
      if ( (document.cookie.indexOf('wanttohide=true') == -1) && (navigator.cookieEnabled) ) {
-		// cookie non détecté
+		// cookie non dï¿½tectï¿½
 		jQuery('#framanavModalAlert').modal({
 			show: $fnav_extra_alert_modal_force
 			});
      } else {
-		 //cookie détécté
+		 //cookie dï¿½tï¿½ctï¿½
 		jQuery('.sub_alert').hide(); 
 	 }
  });
@@ -135,7 +135,7 @@ jQuery(document).ready(function() {
 </script>
 EOAB;
 
-# Si on a défini FNAV_EXTRA_ALERT_TXT_FILES a "true", on charge les fichiers distants
+# Si on a dï¿½fini FNAV_EXTRA_ALERT_TXT_FILES a "true", on charge les fichiers distants
 if ( FNAV_EXTRA_ALERT_TXT_FILES==true ) {
     $fnav_extra_alert_txt_title = @file_get_contents($fnav_extra_alert_txt_files_title);
     $fnav_extra_alert_modal_content = @file_get_contents($fnav_extra_alert_txt_files_content);
