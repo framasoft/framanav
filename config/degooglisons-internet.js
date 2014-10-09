0f878b00a45658933175b58946709e5bc819f219
FramaJosephK <josephk@framasoft.org>
2014-10-09 21:39:09 +0200

    Désactivation du bandeau dio sur dio

--- config/degooglisons-internet.js
+++ config/degooglisons-internet.js
@@ -1,3 +1,8 @@
 f$_config = 'local';
 
+// Force la désactivation des fenêtres modales, du bandeau et du macaron
+f$_alert_text = '';
+f$_alert_modal_text = '';
+f$_modal_don_liendl = '';
+
 f$_footer = true;
