d10a0c6cf989f455df30b0ea1479a2dffe2c4a54
JosephK <josephk@framasoft.org>
2015-01-05 15:36:46 +0100

    Suppression du message d'avertissement

--- config/framapack.js
+++ config/framapack.js
@@ -4,7 +4,4 @@ f$_jquery = 'fQuery';
 
 f$_extra_css = true;
 
-f$_alert_type = 'danger';
-f$_alert_text = '<p style="text-align:center; font-weight:bold">Framapack est en panne depuis la migration du serveur fin juin 2013.<br />Nous ne savons pas quand nous pourrons résoudre le problème. Désolé... :-(</p>';
-
 f$_host = 'online';
