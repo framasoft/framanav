e6a7bcc13df8e73bc46461281ad6bf0312c07fac
www-data <www-data@amber.framasoft.org>
2015-03-06 00:00:40 +0100

    Ajout de la conf lite6

---
+++ config/lite6.framapad.js
@@ -0,0 +1,13 @@
+f$_config = 'local';
+
+f$_jquery = 'fQuery';
+
+f$_extra_css = true;
+
+// Force la désactivation des fenêtres modales, du bandeau et du macaron
+f$_alert_text = '';
+f$_modal_don_liendl = '';
+f$_donate = false;
+
+f$_host = 'hetzner';
+f$_credits = 'framapad';
