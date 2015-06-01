16958417234c3a5154a995aedbb3f9e374a40c09
JosephK <josephk@framasoft.org>
2015-06-01 20:08:43 +0200

    Petit script pour ajouter la somme des votes « Si nécessaire » en attendant la mise en prod de Framadate Openbar

--- config/framadate.js
+++ config/framadate.js
@@ -10,7 +10,21 @@ f$_bootstrap_js = 'html';
 // Opt-in
 if(f$_page('infos_sondage.php?choix_sondage')) {
     f$_email_field1 = '#formulaire input[name="adresse"]';
-
 }
 
 f$_footer = true;
+
+// Somme des votes (fonctionnalité de la branche develop)
+if(window.location.host == 'framadate.org') {
+    jQuery(document).ready(function(){
+        if(jQuery('html').attr('lang')=='fr') {
+            jQuery('tr#addition td:eq(0)').append('<br/>'+(jQuery('#tableContainer tbody tr').length-2)+' votants');
+        }
+        jQuery('tr#addition td').each(function(index) {
+            countIfneedbe = jQuery('#tableContainer tbody td.bg-warning[headers]').filter('[headers*="S'+(index-1)+'"],[headers*="H'+(index-1)+'"]').length;
+            if(countIfneedbe>0) {
+                jQuery(this).append('<br/><span class="small text-muted">(+'+countIfneedbe+')</span>');
+            }
+        });
+    });
+}
