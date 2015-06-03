6d68defed5b1a92b4b60e8306301e1cbb0ee9dc2
JosephK <josephk@framasoft.org>
2015-06-03 14:59:48 +0200

    Fix charts

--- config/framadate.js
+++ config/framadate.js
@@ -19,7 +19,7 @@ if(window.location.host == 'framadate.org') {
     jQuery(document).ready(function(){
         if(jQuery('html').attr('lang')=='fr') {
             jQuery('tr#addition td:eq(0)').append('<br/>'+(jQuery('#tableContainer tbody tr').length-2)+' votants');
-            jQuery.getScript('_charts/resultats.js');
+            jQuery.getScript('/_charts/resultats.js');
         }
         jQuery('tr#addition td').each(function(index) {
             countIfneedbe = jQuery('#tableContainer tbody td.bg-warning[headers]').filter('[headers$="S'+(index-1)+'"],[headers$="H'+(index-1)+'"]').length;
