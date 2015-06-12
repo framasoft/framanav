ea9705bab7db7607033f5d9810907e1e4ae875a3
JosephK <josephk@framasoft.org>
2015-06-12 16:11:51 +0200

    Modal don Framacalc

--- config/framacalc.js
+++ config/framacalc.js
@@ -4,7 +4,7 @@ if(f$_page('framacalc.org/_start')) {
     // Si on est sur la page d'accueil
     f$_modal_don_txtdl1 = 'd’utiliser';
     f$_modal_don_txtdl2 = 'créer un calc';
-    f$_modal_don_liendl = 'a.btn-large[href*="/_new"]';
+    f$_modal_don_liendl = 'a[href*="framacalc.org/"]';
     
     f$_footer = "true";
 
@@ -19,7 +19,7 @@ if(f$_page('framacalc.org/_start')) {
 
     // Force la désactivation des fenêtres modales, du bandeau et du macaron
     f$_alert_text = '';
-    f$_alert_modal_title = 'Avertissement';
+    f$_alert_modal_title = 'Avertissements';
     f$_alert_modal_text = "<h3>Suppression automatique</h3><p>Votre calc sera supprimé au bout d'un an d'inactivité (aucun accès, aucune modification pendant un an).<br/>Ceci nous permet d'éviter de faire grossir indéfiniment la base de données.</p><h3>Information</h3><p>Nous avons changé d'instance de Framacalc le 11 février 2015 suite à des bugs répétés. Pour retrouver votre calc, il vous faudra vous rendre sur <span style='white-space: nowrap;'>http://old.framacalc.org/le_nom_de_mon_calc</span></p><p>Pour exporter votre calc au format CSV, il suffit d'ajouter \".csv\" à la fin de l'adresse du calc (<span style='white-space: nowrap;'>https://framacalc.org/le_nom_de_mon_calc.csv</span>, <span style='white-space: nowrap;'>http://old.framacalc.org/le_nom_de_mon_calc.csv</span> pour l'ancienne instance)</p>";
     f$_modal_don_liendl = '';
     f$_donate = false;
@@ -27,4 +27,3 @@ if(f$_page('framacalc.org/_start')) {
 }
 
 f$_host = 'ovh';
-
