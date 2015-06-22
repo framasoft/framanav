4d60fe553724b9917bd96c11e20ddd4414cf71be
JosephK <josephk@framasoft.org>
2015-06-22 09:32:19 +0200

    Config pour Wordpress avec thème Virtue

--- config/contact.framasoft.js
+++ config/contact.framasoft.js
@@ -1,10 +1,9 @@
 f$_config = 'local';
 
-f$_jquery = 'fQuery';
+f$_jquery = 'html';
+f$_bootstrap = 'html';
 
 f$_nav_static = true;
-
-f$_css_position = 'end';
 
 // Opt-in
 f$_email_field1 = '#wpcf7-f24-p5-o1 .wpcf7-email';
