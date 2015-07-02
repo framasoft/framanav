b1d9938e40b2348035a3d952af8a8423ff871117
Quentin Dupont <quentin.dupont@framasoft.org>
2015-07-02 10:55:47 +0200

    update config Framastats

--- config/framastats.js
+++ config/framastats.js
@@ -4,7 +4,7 @@ f$_jquery = 'html';
 f$_bootstrap_js = 'html';
 
 
-f$_nav_static = false;
+f$_nav_static = true;
 
 f$_footer = true;
 
@@ -12,9 +12,3 @@ f$_footer = true;
 f$_modal_don_txtdl1 = 'd\'utiliser';
 f$_modal_don_txtdl2 = 'accéder au site';
 f$_modal_don_liendl = 'onstart';
-
-// Opt-in
-f$_email_field1 = '';
-f$_email_field2 = '';
-f$_optin_cookie_name = 'opt-in';
-f$_optin_cookie = 365*24*60*60*1000;            // Expire au bout d'un an
