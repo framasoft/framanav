deea2d46d1202b4813fec2f213a55e1f7bb8bed1
Quentin Dupont <quentin.dupont@framasoft.org>
2015-07-02 10:03:21 +0200

    config for framastats

--- config/framastats.js
+++ config/framastats.js
@@ -1,11 +1,15 @@
 f$_config = 'local';
 
+f$_jquery = 'html';
+f$_bootstrap_js = 'html';
+
+
 f$_nav_static = false;
 
 f$_footer = true;
 
 // Popup de don
-f$_modal_don_txtdl1 = 'd'utiliser';
+f$_modal_don_txtdl1 = 'd\'utiliser';
 f$_modal_don_txtdl2 = 'accéder au site';
 f$_modal_don_liendl = 'onstart';
 
