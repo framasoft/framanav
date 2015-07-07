842a6e213c65ac74f2b593ccca192e9ac05067b6
JosephK <josephk@framasoft.org>
2015-07-07 17:08:36 +0200

    Config Framapad

--- config.js
+++ config.js
@@ -308,8 +308,7 @@ switch (f$_site) {
         f$_modal_don_liendl = 'onstart';
     break;
     case 'framapad':
-        f$_jquery = 'fQuery';
-        f$_css_position = 'end';
+        f$_jquery = 'html';
         f$_modal_don_txtdl1 = 'd’utiliser';
         f$_modal_don_txtdl2 = 'créer un pad';
         f$_modal_don_liendl = 'a[href*=".framapad.org/p/"]';
