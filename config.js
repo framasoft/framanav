93732c336749f8cc3db4ac84ecb040a7bfda6a6c
JosephK <josephk@framasoft.org>
2015-09-04 14:10:08 +0200

    Pfff… faut pas se fier à un copier coller :)

--- config.js
+++ config.js
@@ -332,7 +332,7 @@ switch (f$_site) {
         f$_alert_type = 'warning';
         f$_alert_text =
             '<div style="margin:0 auto; max-width:800px;">'+
-                '<p class="text-center">L’instance <b>mensuel</b>.framapad.org rencontre des difficultés pour synchroniser les données.<br/> Nous efforçons de résoudre ce problème.'+
+                '<p class="text-center">L’instance <b>mensuel</b>.framapad.org rencontre des difficultés pour synchroniser les données.<br/> Nous nous efforçons de résoudre ce problème.'+
                 '<br/>Plus d’informations sur <a href="https://status.framasoft.org">status.framasoft.org</a> ou en suivant le hashtag #framasoft sur <a href="https://framasphere.org/tags/framasoft">Diaspora</a>/<a href="https://twitter.com/hashtag/framasoft">Twitter</a>.</p>'+
             '</div>';
         f$_video_js = true;
