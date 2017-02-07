b77e4fa72e79f11e4c41eaaa957bf86fe3a46637
JosephK <josephk@framasoft.org>
2017-02-07 09:48:14 +0100

    Bouton Framaestro sur Framapad

--- nav.js
+++ nav.js
@@ -48,6 +48,19 @@ var n$ = {
   n$.site = (/Framatech/i.test(window.navigator.userAgent)) ? 'agent' : n$.site;     // Config locale
 
   if (n$.inframe) n$.nav.html = '<div id="framanav_container" style="display:none"></div>';
+  if (!n$.inframe) {
+    f$Md5=function(){for(var m=[],l=0;64>l;)m[l]=0|4294967296*Math.abs(Math.sin(++l));return function(c){var e,g,f,a,h=[];c=unescape(encodeURI(c));for(var b=c.length,k=[e=1732584193,g=-271733879,~e,~g],d=0;d<=b;)h[d>>2]|=(c.charCodeAt(d)||128)<<8*(d++%4);h[c=16*(b+8>>6)+14]=8*b;for(d=0;d<c;d+=16){b=k;for(a=0;64>a;)b=[f=b[3],(e=b[1]|0)+((f=b[0]+[e&(g=b[2])|~e&f,f&e|~f&g,e^g^f,g^(e|~f)][b=a>>4]+(m[a]+(h[[a,5*a+1,3*a+5,7*a][b]%16+d]|0)))<<(b=[7,12,17,22,5,9,14,20,4,11,16,23,6,10,15,21][4*b+a++%4])|f>>>32-b),e,g];for(a=4;a;)k[--a]=k[a]+b[a]}for(c="";32>a;)c+=(k[a>>3]>>4*(1^a++&7)&15).toString(16);return c}}();
+    tonality = f$Md5(n$.url).substring(0,8);
+    concerto = n$.url.substring(n$.url.lastIndexOf('/')+1).replace(/[^a-zA-Z0-9=\?]/g, '');
+    currentW = parseInt((window.innerWidth - 60) * 2/3);
+    currentH = parseInt(window.innerHeight - 160);
+    talkW = parseInt(currentW / 2);
+    talkH = parseInt(talkW * 9/16);
+    n$.maestro =
+      'https://framaestro.org/p/#'+tonality+'/'+concerto+'/'+
+      '0,20,'+currentW+','+currentH+','+encodeURIComponent(n$.url)+';'+
+      '0,'+(currentW + 40)+','+talkW+','+talkH+','+encodeURIComponent('https://framatalk.org/'+tonality+concerto)+';';
+  }
 
   if (window.jQuery === undefined) {
     n$.j$ = 'ø';    // n° version du jQuery local
