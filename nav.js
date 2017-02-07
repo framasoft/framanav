04e105b1799aa6e2bd99c63333bca3eb1962fbb7
JosephK <josephk@framasoft.org>
2017-02-07 10:09:35 +0100

    Suppression des hash et querystring du maestro

--- nav.js
+++ nav.js
@@ -16,7 +16,7 @@
  **/
 
 var n$ = {
-  version: '170201', // n° version de la nav
+  version: '170207', // n° version de la nav
   f$ : '1.12.4',     // n° version de notre jQuery
   b$ : '3.3.6',      // n° version de Bootsrap
   host: window.location.host,
@@ -51,7 +51,7 @@ var n$ = {
   if (!n$.inframe) {
     f$Md5=function(){for(var m=[],l=0;64>l;)m[l]=0|4294967296*Math.abs(Math.sin(++l));return function(c){var e,g,f,a,h=[];c=unescape(encodeURI(c));for(var b=c.length,k=[e=1732584193,g=-271733879,~e,~g],d=0;d<=b;)h[d>>2]|=(c.charCodeAt(d)||128)<<8*(d++%4);h[c=16*(b+8>>6)+14]=8*b;for(d=0;d<c;d+=16){b=k;for(a=0;64>a;)b=[f=b[3],(e=b[1]|0)+((f=b[0]+[e&(g=b[2])|~e&f,f&e|~f&g,e^g^f,g^(e|~f)][b=a>>4]+(m[a]+(h[[a,5*a+1,3*a+5,7*a][b]%16+d]|0)))<<(b=[7,12,17,22,5,9,14,20,4,11,16,23,6,10,15,21][4*b+a++%4])|f>>>32-b),e,g];for(a=4;a;)k[--a]=k[a]+b[a]}for(c="";32>a;)c+=(k[a>>3]>>4*(1^a++&7)&15).toString(16);return c}}();
     tonality = f$Md5(n$.url).substring(0,8);
-    concerto = n$.url.substring(n$.url.lastIndexOf('/')+1).replace(/[^a-zA-Z0-9=\?]/g, '');
+    concerto = n$.url.split(/[?#]/)[0].substring(n$.url.lastIndexOf('/')+1).replace(/[^a-zA-Z0-9=\?]/g, '');
     currentW = parseInt((window.innerWidth - 60) * 2/3);
     currentH = parseInt(window.innerHeight - 160);
     talkW = parseInt(currentW / 2);
