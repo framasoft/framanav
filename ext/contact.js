6ad952fcae1b827f98cbbe623046350c1584ba9b
JosephK <josephk@framasoft.org>
2017-03-08 21:09:18 +0100

    [contact] langues du navigateur dans le formulaire

--- ext/contact.js
+++ ext/contact.js
@@ -1,6 +1,8 @@
 jQuery(document).ready(function(){
   // Referrer
   jQuery('input[name="your-referrer"]').val(document.referrer);
+  // Languages
+  jQuery('input[name="your-languages"]').val(window.navigator.languages);
   // Liste des projets
   var list = [
     {col: 0, f:"Frama",   s:"libre",      c:"b", fa:"fa-linux",                 title:"Annuaire de logiciels libres"},
