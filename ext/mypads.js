8812efb97ddd8a11b81e2d1c7c0b195000e0065c
JosephK <josephk@framasoft.org>
2017-07-12 15:33:44 +0200

    [mypads] rohlàlà…

--- ext/mypads.js
+++ ext/mypads.js
@@ -32,9 +32,9 @@ setInterval(function() {
                 '</button>'
             ).css('border-radius','0 5px 0 0');
 
-            var iframeFP = f$Cookie('r', 'iframefp');
+            var iframeFP = f$Cookie('r', 'iframeFP');
 
-            if(iframeFP == 'true') {
+            if(iframeFP == true) {
                 expandIframe();
             } else {
                 compressIframe();
@@ -44,10 +44,10 @@ setInterval(function() {
 
                 if($('.btn.expand-toggle i').hasClass('glyphicon-resize-full')) {
                     expandIframe();
-                    f$Cookie('w','iframeFP','true',365*24*60*60*1000);
+                    f$Cookie('w','iframeFP',true,365*24*60*60*1000);
                 } else {
                     compressIframe();
-                    f$Cookie('w','iframeFP','false',365*24*60*60*1000);
+                    f$Cookie('w','iframeFP',false,365*24*60*60*1000);
                 }
 
             });
