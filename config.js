0f2d9cf38efb75335c5180af424868e605c6c29e
JosephK <josephk@framasoft.org>
2018-02-14 19:08:47 +0100

    Fix erreurs JSHint

--- config.js
+++ config.js
@@ -20,13 +20,13 @@ switch (n$.site) {
   case 'trouvons' : n$.site = 'bee'; break;
 }
 // Config pour domaine et sous-domaine
-if (i$(/framaboard/i, 'h'))        { n$.site = 'board' }
-if (i$(/framadate/i, 'h'))         { n$.site = 'date' }
+if (i$(/framaboard/i, 'h'))        { n$.site = 'board'; }
+if (i$(/framadate/i, 'h'))         { n$.site = 'date'; }
 if (i$(/framacalc/i, 'h'))         { n$.site = 'calc'; }
 
-if (i$('mypads.framapad.org', 'h')) { n$.site = 'mypads' }
-if ( (i$(/.framapad/i, 'h') && !i$(/mypads./i, 'h'))
-    || (i$(/mypads.framapad/i, 'h') && i$('/p/')) ) {
+if (i$('mypads.framapad.org', 'h')) { n$.site = 'mypads'; }
+if ( (i$(/.framapad/i, 'h') && !i$(/mypads./i, 'h')) ||
+     (i$(/mypads.framapad/i, 'h') && i$('/p/')) ) {
   n$.site = 'etherpad';
 }
 
@@ -47,12 +47,12 @@ switch (n$.site) {
         footer: false,
         fixed: i$('fixed'),
         credits: 'pad'
-      }
+      };
       // test/medias.html
       if(i$('media')) {
         l$ = {
           js: { video: true, audio: true, ext: true },
-        }
+        };
       }
       // test/verbose.*.html
       if(i$('verbose')) {
@@ -63,7 +63,7 @@ switch (n$.site) {
             don: ['onstart', 'd’utiliser','créer un pad'],
             info: [
               'Veggie ipsum',
-              '<p>Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip. Sea lettuce lettuce water chestnut eggplant winter purslane fennel azuki bean earthnut pea sierra leone bologi leek soko chicory celtuce parsley jÃ­cama salsify.</p>'+
+              '<p>Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>'+
               '<p>Celery quandong swiss chard chicory earthnut pea potato. Salsify taro catsear garlic gram celery bitterleaf wattle seed collard greens nori. Grape wattle seed kombu beetroot horseradish carrot squash brussels sprout chard.<p>'
             ]
           },
@@ -72,13 +72,13 @@ switch (n$.site) {
             'info',
             'Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.'
           ]
-        }
+        };
       }
     }
   break;
 
   case 'forum':
-    n$.name = 'Framagora'
+    n$.name = 'Framagora';
     l$ = {
       css: { order: '102345' },
       optin: ['#email_confirm', '#email'],
@@ -87,7 +87,7 @@ switch (n$.site) {
           'Fermeture de Framagora',
           '<p>Après 15 années d’existence, le forum historique de Framasoft, ferme ses portes. Pour les nostalgiques et les curieux, il reste toujours possible de consulter les discussions mais c’est maintenant le forum <a href="https://framacolibri.org" style="text-decoration:none"><b class="violet">Frama</b><b class="jaune">colibri</b></a> qui prend la relève.</p>'+
           '<p>Nous avions mis en place <a href="https://framacolibri.org" style="text-decoration:none"><b class="violet">Frama</b><b class="jaune">colibri</b></a> en 2015 pour permettre aux bénévoles souhaitant participer aux projets de Framasoft de s’organiser sur un forum vierge et moderne. Aujourd’hui, la dynamique est bien là.</p>'+
-          '<p>Nous y avons donc reporté les quelques catégories de Framagora qui étaient encore un peu actives :<p><ul>'+
+          '<p>Nous y avons donc reporté les quelques catégories de Framagora qui étaient encore un peu actives&nbsp;:<p><ul>'+
             '<li><a href="https://framacolibri.org/c/framasoft-vous/cherche-logiciel-libre-pour">Cherche logiciel libre pour…</a></li>'+
             '<li><a href="https://framacolibri.org/c/framasoft-vous/ask-frama">Questions / réponses</a></li>'+
             '<li><a href="https://framacolibri.org/c/qualite/framakey">Framakey</a></li>'+
@@ -98,19 +98,23 @@ switch (n$.site) {
         ]
       },
       footer: false
-    }
+    };
   break;
 
   case 'bag':
-    l$ = { mute: i$('framabag.org/u') }
+    l$ = { mute: i$('framabag.org/u') };
   break;
 
   case 'bee':
     l$ = {
       js: {
-        ext: function() { if( jQuery('.explain').length ) { jQuery('#q').focus() }; jQuery('.footer').hide(); jQuery('body').css('margin-bottom','0') }
+        ext: function() { 
+          if( jQuery('.explain').length ) { jQuery('#q').focus(); }
+          jQuery('.footer').hide();
+          jQuery('body').css('margin-bottom','0');
+        }
       }
-    }
+    };
   break;
 
   case 'bin':
@@ -119,7 +123,7 @@ switch (n$.site) {
       modal: {
         don: ['onstart', 'd’utiliser', 'utiliser '+n$.name]
       }
-    }
+    };
   break;
 
   case 'blog':
@@ -127,7 +131,7 @@ switch (n$.site) {
       js: { audio: true, video: true },
       optin: ['#commentform #email'],
       fixed : true
-    }
+    };
   break;
 
   case 'board':
@@ -148,7 +152,7 @@ switch (n$.site) {
         css: { order: '015234', b$: false, ext: true },
         footer: false,
         mute: true
-      }
+      };
     } else {
       l$.optin = ['#registration #email'];
     }
@@ -158,19 +162,19 @@ switch (n$.site) {
     l$ = {
       fixed : true,
       modal: { don: ['a[href*="download-monitor/download.php?id="]'] }
-    }
+    };
   break;
 
   case 'bookin':
     l$ = {
       js: {
-        ext: function() { jQuery('.ui-page').css('margin-top','-42px') }
+        ext: function() { jQuery('.ui-page').css('margin-top','-42px'); }
       },
       css: { ext: i$('framabookin.org/b') },
       mute: i$('framabookin.org/b'),
       fixed: true,
       footer: !i$('framabookin.org/b')
-    }
+    };
   break;
 
   case 'bot' :
@@ -189,18 +193,18 @@ switch (n$.site) {
       // Si on est sur la page d'accueil
       l$.modal = {
         don: ['a[href*="framacalc.org/"]', 'd’utiliser', 'créer un calc']
-      }
-      l$.js = {ext : function(){ jQuery('#drop label:eq(0)').hide(); }}
+      };
+      l$.js = {ext : function(){ jQuery('#drop label:eq(0)').hide(); }};
     } else if (i$('lite.framacalc.org/_start') || i$('https://lite.framacalc.org/', 'u')) {
       // Si on est sur la page d'accueil
       l$.modal = {
         don: ['a[href*="lite.framacalc.org/"]', 'd’utiliser', 'créer un calc']
-      }
-      l$.js = {ext : function(){ jQuery('#drop label:eq(0)').hide(); }}
+      };
+      l$.js = {ext : function(){ jQuery('#drop label:eq(0)').hide(); }};
     } else { // dans Ethercalc
       l$ = {
         js: {
-          ext: function() { jQuery(window).trigger('resize') }
+          ext: function() { jQuery(window).trigger('resize'); }
         },
         css: {
           b$: !n$.inframe,
@@ -210,7 +214,7 @@ switch (n$.site) {
         mute: true,
         footer: false,
         host: 'ovh'
-      }
+      };
     }
   break;
 
@@ -233,7 +237,7 @@ switch (n$.site) {
           jQuery('body').css('background-color', '#eee');
         }
       }
-    }
+    };
   break;
 
   case 'colibri':
@@ -241,7 +245,7 @@ switch (n$.site) {
       css: { ext: true },
       mute: true,
       footer: false
-    }
+    };
   break;
 
   case 'connard' :
@@ -250,7 +254,7 @@ switch (n$.site) {
       mute: true,
       icons: { keep: true },
       footer: false
-    }
+    };
   break;
 
   case 'contact':
@@ -259,19 +263,21 @@ switch (n$.site) {
       css: { ext: true },
       fixed: true,
       optin: ['#wpcf7-f24-p5-o1 .wpcf7-email'],
-    }
+    };
   break;
 
   case 'date':
     l$.js = { ext: i$('framadate.org', 'h') };
-    if(i$Lang('fr')) {
+    if( i$Lang('fr') ) {
       l$.modal = { don: ['a[href*="create_poll.php?"]', 'd’utiliser', 'créer un sondage'] };
-      if(i$('create_poll.php?')) l$.optin ['#formulaire input#email'];
+      if( i$('create_poll.php?') ) { 
+        l$.optin = ['#formulaire input#email'];
+      }
     }
   break;
 
   case 'degooglisons-internet':
-    l$ = { mute: true }
+    l$ = { mute: true };
   break;
 
   case 'dvd':
@@ -279,13 +285,13 @@ switch (n$.site) {
       js: { video : true },
       modal: { don: ['a[href*="iso.framadvd.org"]'] },
       fixed: true
-    }
+    };
   break;
 
   case 'drive':
     l$ = {
       fixed: true,
-    }
+    };
     if( (i$('index.php/app') && !i$('registration')) || i$('index.php/setting') ) {
       l$.footer = false;
       l$.mute = true;
@@ -319,7 +325,7 @@ switch (n$.site) {
       },
       fixed: true,
       modal: { don: ['a[href*="files.framakey.org"]'] }
-    }
+    };
   break;
 
   case 'libre':
@@ -341,7 +347,7 @@ switch (n$.site) {
         '<a href="https://framablog.org/2017/03/21/framalibre-lannuaire-du-libre-renait-entre-vos-mains/" title="consulter l’annonce sur le Framablog">fait peau neuve</a>.<br>'+
         'Certains liens prééxistants ne sont plus valides. <a href="https://contact.framasoft.org/foire-aux-questions/#libre_v2">Vous avez du mal à vous y retrouver ?</a>'
       ]
-    }
+    };
   break;
 
   case 'link':
@@ -350,7 +356,7 @@ switch (n$.site) {
       modal: {
         don : ['onstart', 'd’utiliser', 'utiliser '+n$.name]
       }
-    }
+    };
   break;
 
   case 'listes':
@@ -370,14 +376,14 @@ switch (n$.site) {
         'Ces emails ne sont plus transmis aux personnes concernées et génèrent un trafic inutile sur nos serveurs.<br>'+
         'Merci de prendre vos dispositions pour que les membres concernés de vos listes s’y inscrivent sous une nouvelle adresse email, le grand nettoyage commencera le 6 novembre 2017 à partir de 8h.'
       ]*/
-    }
+    };
   break;
 
   case 'maestro':
     l$ = {
      js: { b$: 'html'},
       mute: i$('/p/')
-    }
+    };
   break;
 
   case 'mindmap':
@@ -387,16 +393,16 @@ switch (n$.site) {
         css: { ext: true },
         mute: true,
         footer: false
-      }
+      };
     } else { // Dans Wisemapping (et accueil)
       l$ = {
         js: { video: true },
         css: { b$: false, ext: true },
         optin: ['#user #email'],
         mute: (!i$('framindmap.org/c/login') && !i$('framindmap.org/c/user/registration'))
-      }
+      };
       if(i$('framindmap.org/c/maps/') && !i$('/edit')) {
-        l$.modal = { don: ['onstart', 'd’utiliser', 'utiliser '+n$.name] }
+        l$.modal = { don: ['onstart', 'd’utiliser', 'utiliser '+n$.name] };
       }
       if(i$('framindmap.org/c/maps') && i$('/edit')) {
         // [Fix] Suppression de la nav dans l'éditeur
@@ -421,7 +427,7 @@ switch (n$.site) {
         js: {
           ext: function() {
             jQuery('#loginform').append(
-              '<p class="alert alert-warning"><b>Rappel :</b> MyFrama sert à '+
+              '<p class="alert alert-warning"><b>Rappel&nbsp;:</b> MyFrama sert à '+
               'regrouper en un même endroit vos liens (notament vos pads, calcs, sondages, etc). '+
               'Il ne permet <strong>pas de créer un compte unique</strong> pour '+
               'accéder à l’ensemble des services de Framasoft.</p>'
@@ -430,7 +436,7 @@ switch (n$.site) {
         },
         mute: true,
         footer: false
-      }
+      };
     } else {
       l$ = {
         js: {
@@ -442,7 +448,7 @@ switch (n$.site) {
             }
           }
         }
-      }
+      };
     }
   break;
 
@@ -450,12 +456,12 @@ switch (n$.site) {
     if(i$('framanews.org/ttrss')) {
       l$ = {
         js: {
-          ext: function() { jQuery(window).trigger('resize') }
+          ext: function() { jQuery(window).trigger('resize'); }
         },
         css: { ext: true },
         mute: true,
         footer: false
-      }
+      };
     }
   break;
 
@@ -464,7 +470,7 @@ switch (n$.site) {
       modal: { don: ['onstart', 'd’utiliser','utiliser '+n$.name] },
       mute: i$('/admin'),
       footer: !i$('/admin')
-    }
+    };
   break;
 
 //-- <framapad> --------------------------------------------------------
@@ -472,7 +478,7 @@ switch (n$.site) {
     l$ = {
       js: { video: true },
       modal: { don: ['a[href*=".framapad.org/p/"]', 'd’utiliser','créer un pad'] }
-    }
+    };
   break;
 
   case 'mypads':
@@ -484,7 +490,7 @@ switch (n$.site) {
       mute: true,
       footer: false,
       credits: 'pad'
-    }
+    };
   break;
 
   case 'etherpad': // dans Etherpad
@@ -502,7 +508,7 @@ switch (n$.site) {
                   '</a></li>');
                 clearInterval(addMaestroBtn);
               }
-            }, 1000)
+            }, 1000);
           }
         }
       },
@@ -513,7 +519,7 @@ switch (n$.site) {
       mute: true,
       footer: false,
       credits: 'pad'
-    }
+    };
     if(i$(/(beta.framapad)/i, 'h')) {
       l$.modal = {
         info: [
@@ -522,7 +528,7 @@ switch (n$.site) {
           '<p>Pensez à utiliser régulièrement la fonction d’export pendant vos tests.</p>'+
           '<p>Merci.<br />L’équipe technique</p>'
         ]
-      }
+      };
     }
     /*if( i$(/(lite.framapad|lite[2-5].)/i, 'h')) {
       l$.modal = {
@@ -549,7 +555,7 @@ switch (n$.site) {
         ext: true
       },
       footer: false
-    }
+    };
   break;
 
   case 'piaf':
@@ -557,14 +563,14 @@ switch (n$.site) {
       js: { ext: true },
       footer: i$('/about'),
       mute: !i$('/about')
-    }
+    };
   break;
 
   case 'pic':
     l$ = {
       js: { video: true },
       modal: { don: ['onstart', 'd’utiliser','utiliser '+n$.name] }
-    }
+    };
   break;
 
   case 'pootle':
@@ -575,7 +581,7 @@ switch (n$.site) {
         ext: true
       },
       mute: true
-    }
+    };
   break;
 
   case 'pouhiou': // pouhiou.com + noenaute.fr
@@ -584,7 +590,7 @@ switch (n$.site) {
       mute: true,
       footer: false,
       icons: { keep: true }
-    }
+    };
   break;
 
   case 'site':
@@ -596,7 +602,7 @@ switch (n$.site) {
         'Le service fonctionne, mais n’est pas encore facile à utiliser par quiconque. '+
         'C’est à l’écoute de vos retours que nous allons l’améliorer et le documenter au cours des semaines à venir.'
       ]
-    }
+    };
   break;
 
   case 'soft':
@@ -606,7 +612,7 @@ switch (n$.site) {
         fav: 'favicon-violet.png',
         apple: 'soft.png'
       }
-    }
+    };
   break;
 
   case 'sphere':
@@ -615,7 +621,7 @@ switch (n$.site) {
         ext: function() {
             jQuery('link[href*=bootstrap-complete]').remove();
             jQuery("#inscription-email").text(function(){
-                return jQuery(this).text().replace('inscription-framasphere@framalistes.org', 'rt+framasphere@framasoft.org')
+                return jQuery(this).text().replace('inscription-framasphere@framalistes.org', 'rt+framasphere@framasoft.org');
             });
             jQuery.getJSON( "https://framasphere.org/nodeinfo/2.0" ).done( function( data ) {
                 jQuery('#userFramasphere').text(data.usage.users.total);
@@ -623,11 +629,11 @@ switch (n$.site) {
         }
       },
       css: { order: '102345' }
-    }
+    };
   break;
 
   case 'stats':
-    l$ = { fixed: true }
+    l$ = { fixed: true };
   break;
 
 
@@ -637,7 +643,7 @@ switch (n$.site) {
         ext: function() {
           jQuery('.section-components:eq(0) li.sub-component').each(function() {
             var framatruc = jQuery(this).children('a');
-            framatruc.after('<span class="col-xs-1 pull-right"><a href="https://contact.framasoft.org/#'+framatruc.text().split(' ', 1)[0].toLowerCase().replace(/è/g, 'e')+'" class="btn btn-success btn-outline btn-xs" title="Signaler une panne concernant '+framatruc.text()+'"><i class="fa fa-fw fa-lg fa-bug" aria-hidden="true"></i><span class="sr-only">Signaler</span></a></span>')
+            framatruc.after('<span class="col-xs-1 pull-right"><a href="https://contact.framasoft.org/#'+framatruc.text().split(' ', 1)[0].toLowerCase().replace(/è/g, 'e')+'" class="btn btn-success btn-outline btn-xs" title="Signaler une panne concernant '+framatruc.text()+'"><i class="fa fa-fw fa-lg fa-bug" aria-hidden="true"></i><span class="sr-only">Signaler</span></a></span>');
           });
           jQuery('.section-components:eq(0) li.sub-component a.btn[title]').tooltip();
           jQuery('.section-status').append('<p class="well"><i class="fa fa-warning" aria-hidden="true"></i> Le statut des services n’est pas détecté automatiquement. Cette page est actualisée manuellement par l’équipe technique lorsqu’un incident est constaté ou qu’une opération de maintenance est programmée afin d’en informer le public. Si un service vous semble en panne mais n’est pas indiqué comme tel ici, merci de <a href="https://contact.framasoft.org/#aide">nous le signaler</a>.</p>');
@@ -646,7 +652,7 @@ switch (n$.site) {
       mute: true,
       footer: false,
       fixed: true
-    }
+    };
   break;
 
   case 'team':
@@ -657,7 +663,7 @@ switch (n$.site) {
       mute: true,
       donate: false,
       footer:false
-    }
+    };
   break;
 
   case 'tube':
@@ -666,7 +672,7 @@ switch (n$.site) {
       js: { video: true },
       host: 'ovh',
       mute: i$('/embed_player')
-    }
+    };
   break;
 
   case 'vectoriel':
@@ -678,12 +684,12 @@ switch (n$.site) {
         },
         mute: true,
         footer: false
-      }
+      };
     } else {
       l$ = {
         js: { video: true },
         modal: { don: ['a[href="/svg-editor.html"]', 'd’utiliser', 'créer une image'] }
-      }
+      };
     }
   break;
 
@@ -692,7 +698,7 @@ switch (n$.site) {
       js: { video: i$('https://framavox.org/', 'u') },
       css: { ext: true },
       fixed: !i$('https://framavox.org/', 'u')
-    }
+    };
 
     if( !i$(/(\/users\/sign|\/start_group|users\/password\/new)/i, 'u') && !i$('https://framavox.org/', 'u') ) {
       l$.footer = false;
@@ -715,7 +721,7 @@ switch (n$.site) {
           fav: 'fav_wiki.png',
           apple: 'wiki.png'
         }
-      }
+      };
     } else {
       n$.inframe = i$('mediamanager.php');
       l$ = {
@@ -725,7 +731,7 @@ switch (n$.site) {
           fav: 'fav_wiki2.png',
           apple: 'wiki2.png'
         }
-      }
+      };
     }
   break;
 
@@ -742,7 +748,7 @@ switch (n$.site) {
         }
       },
       fixed: true
-    }
+    };
   break;
 }
 
@@ -826,14 +832,13 @@ switch (n$.site) {
 
 
 if(
-    l$.piwik.id != '' 
-  && 
+    l$.piwik.id != '' && 
     // Pas de Piwik si DoNotTrack 
     // DNT est respecté mais on évite les notifications des uBlock, Ghostery, etc
-    !( navigator.doNotTrack == "yes" 
-    || navigator.doNotTrack == "1" 
-    || navigator.msDoNotTrack == "1" 
-    || window.doNotTrack == "1")
+    !( navigator.doNotTrack == "yes" ||
+    navigator.doNotTrack == "1" ||
+    navigator.msDoNotTrack == "1" ||
+    window.doNotTrack == "1" )
   ) {
   
   // Code Javascript
@@ -887,8 +892,8 @@ if (i$(/(phonie)/i,'h')) {
   l$.icons.fav = 'favicon-jaune.png';
 }
 
-if ( i$(/(agenda|bag|bee|bin|blog|board|bookin|book|calc|carte|cloud|code|colibri|date|drive|drop|dvd|forms|games|key|lab|lang|libre|link|listes|maestro|memo|mindmap|minetest|news|pack|phonie|piaf|pic|site|slides|sphere|start|stats|status|talk|team|tube|vectoriel|vox|zic)/i,'h')
-   || i$(/(bot.|contact.|degooglisons-internet|forum.|participer.|soutenir.)/i,'h') ){
+if ( i$(/(agenda|bag|bee|bin|blog|board|bookin|book|calc|carte|cloud|code|colibri|date|drive|drop|dvd|forms|games|key|lab|lang|libre|link|listes|maestro|memo|mindmap|minetest|news|pack|phonie|piaf|pic|site|slides|sphere|start|stats|status|talk|team|tube|vectoriel|vox|zic)/i,'h') ||
+     i$(/(bot.|contact.|degooglisons-internet|forum.|participer.|soutenir.)/i,'h') ){
   l$.icons.apple = n$.site+'.png';
   l$.icons.fav = 'fav_'+n$.site+'.png';
 }
