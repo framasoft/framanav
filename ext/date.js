960fc9f3fe629cf736f6abb9a222fd1e5831a49f
JosephK <josephk@framasoft.org>
2018-03-02 15:09:57 +0100

    [eslint] date.js

--- ext/date.js
+++ ext/date.js
@@ -1,10 +1,12 @@
+/* global d$:{} f$ n$:{} */
+
 // Tutoriel
 d$.f.date.modal = {
   title: '',
   file: '',
   link: '',
-  or: ''
-}
+  or: '',
+};
 
 if (n$.is.lang('fr')) {
   d$.f.date.modal.title = 'Planifier un rendez-vous rapidement avec Framadate';
@@ -20,9 +22,22 @@ if (n$.is.lang('en')) {
   d$.f.date.modal.or = 'or';
 }
 
-if (d$.f.date.modal.file != '' ) {
-  f$('a[href*="aqg259dth55iuhwm"]:contains("exemple")').before('<a href="'+d$.f.date.modal.file+'">'+d$.f.date.modal.link+'</a> '+d$.f.date.modal.or+' ');
+if (d$.f.date.modal.file !== '') {
+  f$('a[href*="aqg259dth55iuhwm"]:contains("exemple")').before([
+    '<a href="', d$.f.date.modal.file, '">', d$.f.date.modal.link, '</a> ',
+    d$.f.date.modal.or, ' ',
+  ].join(''));
 }
 
-
-f$('head').append('<style>.yes .btn,.yes .btn:hover { color: #677835 !important;}.ifneedbe .btn,.ifneedbe .btn:hover { color: #C48A1B !important;}.no .btn,.no .btn:hover{color: #AD220F !important;}.ifneedbe input[type="radio"]:checked + label,.yes input[type="radio"]:checked + label { color: #fff !important;}.no .btn.startunchecked {color:#AD220F !important;}.no input[type="radio"]:checked + label:not(.startunchecked){color:#fff !important}#message-container .well {display:none;}</style>');
+f$('head').append([ // TODO à déplacer dans frama.scss
+  '<style>',
+  '.yes .btn,.yes .btn:hover { color: #677835 !important;}',
+  '.ifneedbe .btn,.ifneedbe .btn:hover { color: #C48A1B !important;}',
+  '.no .btn,.no .btn:hover{color: #AD220F !important;}',
+  '.ifneedbe input[type="radio"]:checked + label,',
+  '.yes input[type="radio"]:checked + label { color: #fff !important;}',
+  '.no .btn.startunchecked {color:#AD220F !important;}',
+  '.no input[type="radio"]:checked + label:not(.startunchecked){color:#fff !important}',
+  '#message-container .well {display:none;}',
+  '</style>',
+].join(''));
