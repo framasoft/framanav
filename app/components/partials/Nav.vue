db7fb405b2564ee45132d5df0dac528fe7b269c9
JosephK <josephk@framasoft.org>
2018-08-02 17:21:00 +0200

    [WIP] footer

--- app/components/partials/Nav.vue
+++ app/components/partials/Nav.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="framanav_container" class="hidden-print" style="'height:42px;" v-show="!inframe">
     <navbar id="framanav" role="menubar">
-      <a class="navbar-brand" :href="data.meta.baseURL">
-        <img :src="data['/img/'] + 'logo.png'" alt="" />
-        <span class="hidden-sm" v-html="data.meta.framaname"></span>
-      </a>
-      <a href="#nav-end" id="nav-skip">{{ $t('txt.skip') }}</a>
+      <span slot="brand">
+        <a class="navbar-brand" slot="brand" :href="data.meta.baseURL">
+          <img :src="data['/img/'] + 'logo.png'" alt="" />
+          <span class="hidden-sm" v-html="data.meta.framaname"></span>
+        </a>
+        <a href="#nav-end" id="nav-skip">{{ $t('txt.skip') }}</a>
+      </span>
       <template slot="collapse">
         <navbar-nav>
           <dropdown
@@ -16,6 +18,7 @@
               {{ cat.name }} <b class="caret"></b>
             </a>
             <template slot="dropdown">
+              <li class="dropdown-header" v-if="key === 'about'" v-html="data.meta.framaname"></li>
               <popover tag="li" :class="'fs_' + l"
                 :enable="$i18n.messages[$t('lang')].fnav.cat[key].sites[l].t1 ? true : false"
                 :placement="((key === 'services' || key === 'about') && !(index % 2)) ? 'left' : 'right'"
@@ -24,12 +27,14 @@
                 v-for="(site, l, index) in cat.sites" :key="l"
               >
                 <a :href="site.link">
-                  <i :class="'fa fa-lg ' + data.fnav.cat[key].sites[l].icon" aria-hidden="true"></i>
+                  <i :class="'fa fa-fw fa-lg ' + data.fnav.cat[key].sites[l].icon" aria-hidden="true"></i>
                   {{
                     data.fnav.cat[key].sites[l].name
                     ? text(data.fnav.cat[key].sites[l].name)
                     : text($t('fnav.cat.' + key + '.sites.' + l + '.name'))
                   }}
+                  <span class="sr-only">({{ $t('txt.newWindow') }})</span>
+                  <i class="fa new-window fa-external-link" aria-hidden="true"></i>
                 </a>
                 <template slot="popover">
                   <div v-html="$t('fnav.cat.' + key + '.sites.' + l + '.d1')"></div>
@@ -42,7 +47,7 @@
             :title="text($t('fnav.soutenir.t1'))"
           >
             <a :href="$t('fnav.soutenir.link') + '/?f=nav'" class="btn-soutenir">
-              <i :class="'fa fa-lg ' + data.fnav.soutenir.icon" aria-hidden="true"></i>
+              <i :class="'fa fa-fw fa-lg ' + data.fnav.soutenir.icon" aria-hidden="true"></i>
               {{ text($t('fnav.soutenir.name')) }}
             </a>
             <template slot="popover">
@@ -54,7 +59,7 @@
             :title="text($t('fnav.benevalo.t1'))"
           >
             <a :href="$t('fnav.benevalo.link')" class="btn-info">
-              <i :class="'fa fa-lg ' + data.fnav.benevalo.icon" aria-hidden="true"></i>
+              <i :class="'fa fa-fw fa-lg ' + data.fnav.benevalo.icon" aria-hidden="true"></i>
               {{ text($t('fnav.benevalo.name')) }}
             </a>
             <template slot="popover">
@@ -68,7 +73,7 @@
             <a :href="myframa" class="btn-primary"
               @click="window.open(myframa, 'myframa', 'menubar=no,height=500,width=600,toolbar=no,scrollbars=yes,status=no,dialog=1'); return false;"
             >
-              <i :class="'fa fa-lg ' + data.fnav.myframa.icon" aria-hidden="true"></i>
+              <i :class="'fa fa-fw fa-lg ' + data.fnav.myframa.icon" aria-hidden="true"></i>
               {{ text($t('data.fnav.cat.services.sites.my.name')) }}
             </a>
             <template slot="popover">
@@ -83,8 +88,88 @@
       class="hidden-xs">
       <span class="sr-only">{{ text($t('fnav.soutenir.name')) }}</span>
     </a>
-    <footer>
-
+    <footer id="framafooter" class="clearfix hidden-print" role="contentinfo">
+      <div class="container">
+        <div class="clearfix col-sm-8">
+          <nav class="col-xs-4">
+            <h1>{{ data.meta.name }}</h1>
+            <ul class="list-unstyled">
+              <li v-for="key in data.fnav.footer.frama">
+                <a
+                  :href="$t('fnav.cat.about.sites.' + key + '.link')"
+                  v-html="$t('fnav.cat.about.sites.' + key + '.name')"
+                ></a>
+              </li>
+            </ul>
+          </nav>
+          <nav class="col-xs-4">
+            <h1>{{ $t('txt.community') }}</h1>
+            <ul class="list-unstyled">
+              <li>
+                <a :href="data.fnav.cat.follow.sites.colibri.link">
+                  {{ text(data.fnav.cat.follow.sites.colibri.name) }}
+                </a>
+              </li>
+              <li v-for="key in data.fnav.footer.community">
+                <a
+                  :href="$t('fnav.cat.about.sites.' + key + '.link')"
+                  v-html="$t('fnav.cat.about.sites.' + key + '.name')"
+                ></a>
+              </li>
+            </ul>
+          </nav>
+          <nav class="col-xs-4">
+            <h1>{{ $t('txt.site') }}</h1>
+            <ul class="list-unstyled">
+              <li v-for="key in data.fnav.footer.site">
+                <a
+                  :href="$t('fnav.cat.about.sites.' + key + '.link')"
+                  v-html="$t('fnav.cat.about.sites.' + key + '.name')"
+                ></a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div class="col-sm-4">
+          <div class="col-xs-12">
+            <h1>{{ $t('fnav.cat.follow.name') }}</h1>
+            <ul class="list-inline">
+              <popover tag="li" :class="'fs_' + key"
+                v-for="key in data.fnav.footer.follow" :key="key"
+                placement="top" trigger="hover-focus" append-to="#framanav"
+                :title="text($t('fnav.cat.follow.sites.' + key + '.t1'))"
+              >
+                <a :href="data.fnav.cat.follow.sites[key].link"
+                  data-toggle="popover" data-content="Notre compte sur le réseau social libre Diaspora*"
+                  data-placement="top"
+                  data-original-title="Framasoft sur Diaspora*">
+                  <i :class="'fa fa-fw fa-2x ' + data.fnav.cat.follow.sites[key].icon" aria-hidden="true"></i>
+                  <span class="sr-only" v-html="$t('fnav.cat.follow.sites.' + key + '.name')"></span>
+                </a>
+                  <template slot="popover">
+                  <div v-html="$t('fnav.cat.follow.sites.' + key + '.d1')"></div>
+                </template>
+              </popover>
+            </ul>
+            <h2 v-html="$t('fnav.cat.follow.sites.newsletter.name')"></h2>
+            <form action="https://contact.framasoft.org/php_list/lists/?p=subscribe&amp;id=2"
+              method="post" name="subscribeform">
+              <div class="input-group input-group-sm">
+                <input class="form-control" :title="$t('txt.typeYourEmail')" name="email" size="40" :placeholder="$t('txt.yourEmail')" type="text">
+                <span class="input-group-btn">
+                  <button class="btn btn-default" name="subscribe" type="submit" value="subscribe">
+                    {{ $t('txt.subscribe') }}<span class="sr-only"> {{ $t('txt.toTheNewsletter') }}</span>
+                  </button>
+                </span>
+              </div>
+              <input name="htmlemail" value="1" type="hidden">
+              <input name="list[5]" value="signup" type="hidden">
+              <input name="listname[5]" value="Newsletter" type="hidden">
+              <div style="display: none;"><input name="VerificationCodeX" size="20" value="" type="text"></div>
+            </form>
+          </div>
+        </div>
+      </div>
     </footer>
   </div>
 </template>
