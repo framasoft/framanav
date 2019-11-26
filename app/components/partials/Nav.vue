98764af141fd65b3930b956bcac0f34c3cb6e13a
JosephK <josephk@framasoft.org>
2019-11-26 23:18:44 +0100

    [team] seems ok

--- app/components/partials/Nav.vue
+++ app/components/partials/Nav.vue
@@ -13,7 +13,7 @@
 
     <Framabin v-if="/:\/\/framabin.org.p/.test($t('url'))" />
     <Framavox v-if="/:\/\/framavox.org/.test($t('url'))" />
-    <Framateam v-if="/:\/\/framateam.org/.test($t('url')) && testing.team" />
+    <Framateam v-if="/:\/\/framateam.org/.test($t('url'))" />
 
     <Optin v-if="cortexReady" />
     <FooterMenu />
@@ -126,7 +126,6 @@ export default {
       kkeys.push(e.keyCode);
       if (/65,65,66,66/.test(kkeys.toString())) {
         this.testing.feedback = true;
-        this.testing.team = true;
       }
     }, true);
   },
